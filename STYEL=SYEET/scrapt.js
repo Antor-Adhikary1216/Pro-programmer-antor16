87135068da81490ce358626635b9b0607c669715
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2025-11-13 22:27:25 +0530

    complete all our website

---
+++ STYEL=SYEET/scrapt.js
@@ -0,0 +1,8 @@
+//  SCRAPING STERD
+const menuIcon= document.querySelector('.menu') ;
+const navList= document.querySelector('.nav-list');
+menuIcon.addEventListener('click', function(){
+    .navList.classList.toggle('active');
+
+
+})
